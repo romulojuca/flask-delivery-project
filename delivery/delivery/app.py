969b22e6808c1585878903ebed54b1785a04fb19
Romulo Rueda <romulojuca@hotmail.com>
2022-10-24 13:06:01 -0300

    make clear

--- delivery/delivery/app.py
+++ delivery/delivery/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+
 from delivery.ext import config
 
 
 def create_app():
-    app = Flask(__name__)#__name__ mesma coisa que delivery/app.py(por convensao usa __name)
+    app = Flask(
+        __name__
+    )  # __name__ mesma coisa que delivery/app.py(por convensao usa __name)
     config.init_app(app)
 
     return app
